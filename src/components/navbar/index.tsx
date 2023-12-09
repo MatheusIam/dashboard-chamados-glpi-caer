47f3dcaa7078c032dab5eb4a8392de77ed47b91a
Matheus Iam <matheusiam27@gmail.com>
2023-12-09 15:58:57 -0400

    initial commit

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+} from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Mapa de chamados
+          </Typography>
+          <Button color="inherit">Dashboard</Button>
+          <Button color="inherit">Ranking</Button>
+          <Button color="inherit">Rede</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
